9b7a7a8da0711f5a9d3b1253594939447e4eb086
DekkaSwanz <49539804+DekkaSwanz@users.noreply.github.com>
2019-09-27 12:17:40 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
-const absolutify = require('absolutify')
 const app = express();
 
 const Port = process.env.PORT || 5000
@@ -17,7 +16,7 @@ app.get('/',async (req,res)=>{
         await page.goto(`https://${url}`)
 
         let document = await page.evaluate(()=> document.documentElement.outerHTML)
-        document = absolutify(document,`/url=${url.split('/')[0]}`)
+        
         return res.send(document)
         }catch (e){
             return res.send(e)
